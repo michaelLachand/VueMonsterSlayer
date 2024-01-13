df5f6df50e631acaf1b83adc6c731a04410ccb41
wes35 <lachand_michael@hotmail.com>
2024-01-13 20:33:50 +0100

    Implementation specialAttack

--- app.js
+++ app.js
@@ -8,10 +8,23 @@ const app =  Vue.createApp({
        return {
            playerHealth: 100,
            monsterHealth: 100,
+           currentRound: 0,
        };
    },
+    computed: {
+        monsterBarStyles() {
+            return {width: this.monsterHealth + '%'};
+        },
+        playerBarStyles(){
+            return {width: this.playerHealth + '%'};
+        },
+        currentRoundTurn() {
+            return this.currentRound % 3;
+        }
+    },
     methods: {
        attackMonster() {
+           this.currentRound++;
            const attackValue = getRandomValue(5, 12);
            // this.monsterHealth =  this.monsterHealth - attackValue;
            this.monsterHealth -= attackValue;
@@ -21,6 +34,12 @@ const app =  Vue.createApp({
            const attackValue = getRandomValue(8, 15);
            this.playerHealth -= attackValue;
        },
+       specialAttackMonster() {
+           this.currentRound++;
+           const attackValue = getRandomValue(10, 25);
+           this.monsterHealth -= attackValue;
+           this.attackPlayer();
+       }
     }
 });
 
