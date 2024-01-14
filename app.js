8d47e67b2f1d32fac793dc809ef2c6c4ed86507c
wes35 <lachand_michael@hotmail.com>
2024-01-14 22:17:54 +0100

    Implementation Bar, winner and surrender

--- app.js
+++ app.js
@@ -9,20 +9,49 @@ const app =  Vue.createApp({
            playerHealth: 100,
            monsterHealth: 100,
            currentRound: 0,
+           winner: null,
        };
    },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%'};
+            }
             return {width: this.monsterHealth + '%'};
         },
         playerBarStyles(){
+            if (this.playerHealth < 0) {
+                return { width: '0%'};
+            }
             return {width: this.playerHealth + '%'};
         },
         currentRoundTurn() {
             return this.currentRound % 3;
         }
     },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                this.winner = 'player';
+            }
+        }
+    },
     methods: {
+       startGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+       },
        attackMonster() {
            this.currentRound++;
            const attackValue = getRandomValue(5, 12);
@@ -49,6 +78,9 @@ const app =  Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
+        },
+        surrender() {
+           this.winner = 'monster';
         }
     }
 });
